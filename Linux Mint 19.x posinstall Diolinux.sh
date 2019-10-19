c1e8af986a5a003694e75d896279a77a9cc8368e
Fabio Henrique <fabiohm2@gmail.com>
2019-10-19 13:49:20 -0300

    inserindo wine e lutris

--- Linux Mint 19.x posinstall Diolinux.sh
+++ Linux Mint 19.x posinstall Diolinux.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 # ----------------------------- VARIÁVEIS ----------------------------- #
 PPA_GRAPHICS_DRIVERS="ppa:graphics-drivers/ppa"
+PPA_LUTRIS="ppa:lutris-team/lutris"
 
 URL_GOOGLE_CHROME="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+URL_PPA_WINE="https://dl.winehq.org/wine-builds/ubuntu/"
 URL_SIMPLE_NOTE="https://github.com/Automattic/simplenote-electron/releases/download/v1.8.0/Simplenote-linux-1.8.0-amd64.deb"
 URL_INSYNC="https://d2t3ff60b2tol4.cloudfront.net/builds/insync_3.0.20.40428-bionic_amd64.deb"
-
+URL_VAGRANT="https://releases.hashicorp.com/vagrant/2.2.6/vagrant_2.2.6_x86_64.deb"
 DIRETORIO_DOWNLOADS="$HOME/Downloads/programas"
 # ---------------------------------------------------------------------- #
 
@@ -20,8 +22,12 @@ sudo dpkg --add-architecture i386
 ## Atualizando o repositório ##
 sudo apt update -y
 
-## Adicionando repositórios de terceiros e suporte a Snap (Driver Logitech, Lutris e Drivers Nvidia)##
+## Adicionando repositórios de terceiros e suporte a Snap (Lutris e Drivers Nvidia)##
 sudo apt-add-repository "$PPA_GRAPHICS_DRIVERS" -y
+sudo add-apt-repository "$PPA_LUTRIS" -y
+wget -nc "$URL_WINE_KEY"
+sudo apt-key add winehq.key
+sudo apt-add-repository "deb $URL_PPA_WINE bionic main"
 # ---------------------------------------------------------------------- #
 
 # ----------------------------- EXECUÇÃO ----------------------------- #
@@ -35,6 +41,7 @@ mkdir "$DIRETORIO_DOWNLOADS"
 wget -c "$URL_GOOGLE_CHROME"       -P "$DIRETORIO_DOWNLOADS"
 wget -c "$URL_SIMPLE_NOTE"         -P "$DIRETORIO_DOWNLOADS"
 wget -c "$URL_INSYNC"              -P "$DIRETORIO_DOWNLOADS"
+wget -c "$URL_VAGRANT"             -P "$DIRETORIO_DOWNLOADS"
 
 ## Instalando pacotes .deb baixados na sessão anterior ##
 sudo dpkg -i $DIRETORIO_DOWNLOADS/*.deb
@@ -46,6 +53,9 @@ sudo apt install virtualbox -y
 sudo apt install flameshot -y
 sudo apt install nemo-dropbox -y
 sudo apt install steam-installer steam-devices steam:i386 -y
+sudo apt install lutris libvulkan1 libvulkan1:i386 -y
+sudo apt install --install-recommends winehq-stable wine-stable wine-stable-i386 wine-stable-amd64 -y
+sudo apt install libgnutls30:i386 libldap-2.4-2:i386 libgpg-error0:i386 libxml2:i386 libasound2-plugins:i386 libsdl2-2.0-0:i386 libfreetype6:i386 libdbus-1-3:i386 libsqlite3-0:i386 -y
 
 ##Instalando pacotes Flatpak ##
 
